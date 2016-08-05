159ca499425973d0d1a50df46777ef7d018cd237
Adam Caron <adamwcaron@gmail.com>
2016-08-05 16:23:41 -0600

    Up and running

---
+++ app.js
@@ -0,0 +1,22 @@
+
+
+const bills = [
+  {
+    // name: "",
+    // amount: "",
+    // dueDate: "",
+    // auto: false,
+    // accountWithdrawnFrom:
+  }
+]
+
+const income = [
+  // depositDate: "",
+  // amount: ""
+]
+
+function populateIncome (startDate, endDate, defaultDepositAmount) => {
+  //
+}
+
+populateIncome()
