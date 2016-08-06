c17cf7a25df03709a5cc9b77faa4f06bcb86afae
Adam Caron <adamwcaron@gmail.com>
2016-08-05 18:48:36 -0600

    Return this week's income
    with some fun utilities

---
+++ Readme.markdown
@@ -0,0 +1,3 @@
+An app to manage finances.
+
+To test out 'moment.js' stuff: [https://tonicdev.com/npm/moment](https://tonicdev.com/npm/moment) and [http://momentjs.com/docs](http://momentjs.com/docs).
--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-
+const moment = require('moment')
 
 const bills = [
   {
@@ -10,13 +10,49 @@ const bills = [
   }
 ]
 
-const income = [
-  // depositDate: "",
-  // amount: ""
-]
+// Goal:
+// Get me a list of deposits by week for the next relevant month
+// and a total income over time
 
-function populateIncome (startDate, endDate, defaultDepositAmount) => {
-  //
+function income () {
+  const daysOfWeek = {
+       "sunday": 0,
+       "monday": 1,
+      "tuesday": 2,
+    "wednesday": 3,
+     "thursday": 4,
+       "friday": 5,
+     "saturday": 6
+  }
+
+  let payday = daysOfWeek.wednesday
+
+  // if payday already past this week, return next week's payday
+  let nextSoonestPayday = moment().day() > payday ? (payday + 7) : payday
+
+  let depositDate = moment()
+    .day(nextSoonestPayday)
+    .format('M/D'),
+    depositAmount = 1100
+
+  return depositDate + ' + ' + depositAmount
 }
 
-populateIncome()
+console.log( income() )
+
+
+
+// utils
+
+function times (n, f) { while ( n-- > 0 ) f() } // do something for a repeatable number of times
+
+function dollars (amount) {
+  let options = {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2
+  }
+  return (amount).toLocaleString("en-US", options)
+  // http://stackoverflow.com/questions/149055/how-can-i-format-numbers-as-money-in-javascript
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number/toLocaleString#Browser_Compatibility
+}
