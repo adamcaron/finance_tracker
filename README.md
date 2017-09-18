bcc44dff0e48e1ae811479a46348c2d3216a60f6
Adam Caron <adamwcaron@gmail.com>
2017-09-18 14:53:13 -0600

    up and running with ui/api stack

---
+++ README.md
@@ -0,0 +1,18 @@
+# Finance Tracker
+
+## Up and Running
+
+ - `git clone git@github.com:adamcaron/finance_tracker.git`
+ - `cd finance_tracker`
+ - ensure Docker is running
+
+### dev
+
+ - `bash dev-stack.sh`
+ - navigate to yourhostname for app ui
+ - navigate to yourhostname:8080 for proxy dashboard
+
+### prod
+
+ - `bash prod-stack.sh`
+ - navigate to yourhostname for ui
---
+++ api/src/server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 8085
+const router = express.Router()
+router.get('/', (req, res) => {
+  res.json({ message: 'It\'s working' })
+})
+router.get('/auth', (req, res) => {
+  res.json({ message: 'authed' })
+})
+router.get('/listings', (req, res) => {
+  res.json({ message: 'Listings' })
+})
+app.use('/api/', router)
+app.listen(port)
+console.log('Server running on port ' + port)
