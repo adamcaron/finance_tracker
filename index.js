9366835a640f520911d3ab7d3a6834744cc8b4ba
Adam Caron <adamwcaron@gmail.com>
2017-11-02 00:23:12 -0600

    Renders timeline with daily balance given daily transactions

--- index.js
+++ index.js
@@ -1,67 +1,53 @@
 const dollars = require('./utils/dollars')
 const moment = require('moment')
-const seed = require('./utils/seed')
 const db = require('./db')
-seed(db, moment)
-const transactions = db.transactions
+// const seed = require('./utils/seed')
+// seed(db, moment)
+// const transactions = db.transactions
+const transactions = db.recurringTransactions
 
 // TODO: recieve this info via params
 let dateFormat = 'MMM DD' // receive from param
 let startDate = moment(transactions[0].date).format() // param
 let endDate = moment(startDate).add(4, 'months').format() // param
 let numberOfDaysInTimeline = moment(endDate).diff(startDate, 'days')
-let timeline = {}
-let startBalance = 900 // receive from param
+let timeline = []
+let startBalance = 0 // receive from param
 
 // Create the Timeline
 for (let i=0; i<= numberOfDaysInTimeline; i++) {
-  let day = moment(startDate).add(i, 'days').format(dateFormat)
-  timeline[day] = { 'transactions': [], balance: '' }
+  let month = moment(startDate).add(i, 'days').format('MMM')
+  let day = moment(startDate).add(i, 'days').format('DD')
+  timeline.push({
+    transactions: [],
+    balance: 0,
+    month,
+    day
+  })
 }
-
+// Populate Timeline Transactions
+transactions.forEach(transaction => {
+  timeline.forEach(day => {
+    if (day.day === transaction.interval[1]) {
+      day.transactions.push(transaction)
+    }
+  })
+})
+// Sum Transactions
+timeline = timeline.map(day => {
+  day.balance = day.transactions.reduce((acc, t) => {
+    return acc + t.amt
+  }, day.balance)
+  return day
+})
+// Maintain Daily Balance
 let balance = startBalance
-
-// Put transactions in timeline
-transactions.map((transaction) => {
-  let day = moment(transaction.date).format(dateFormat)
-  timeline[day]['transactions'].push(transaction)
-  balance = balance + transaction.amt
-  timeline[day]['balance'] = balance
+timeline = timeline.map(day => {
+  balance = balance + day.balance
+  day.balance = balance
+  return day
 })
 
-balance = startBalance
-console.log(dollars( balance ))
-
-Object.keys(timeline).map((day) => {
-
-  // transaction this day?
-  if (timeline[day].balance) {
-    let transactionsThisDay = timeline[day].transactions.map((transaction) => {
-
-      // deposit?
-      if (transaction.amt > 0) {
-        return '\n          +' + dollars( transaction.amt ) + ' ' + transaction.desc
-      }
-
-      return '\n          ' + dollars( transaction.amt ) + ' ' + transaction.desc
-    })
-
-    return console.log(
-      transactionsThisDay
-      + ' \n\n'
-      + day + ' =>  ' + dollars( timeline[day].balance ) + '_______________'
-    )
-  }
-
-  return console.log( day )
+timeline.forEach(day => {
+  console.log(`${day.month} ${day.day} ${day.balance}`)
 })
-
-// TODO:
-// reduce transactions
-// pass in timeline as initialValue
-// could also pass an accumjlator that has startDate and balance
-
-
-
-
-
